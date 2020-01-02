8380d6557d63d363c4c98d7955011f730679bccf
Me <Me>
2020-01-02 11:09:56 -0800

    Initial Commit

---
+++ YelpHelper/YelpHelper.h
@@ -0,0 +1,19 @@
+//
+//  YelpHelper.h
+//  YelpHelper
+//
+//  Created by dirtbag on 1/2/20.
+//  Copyright © 2020 dirtbag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YelpHelper.
+FOUNDATION_EXPORT double YelpHelperVersionNumber;
+
+//! Project version string for YelpHelper.
+FOUNDATION_EXPORT const unsigned char YelpHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YelpHelper/PublicHeader.h>
+
+
